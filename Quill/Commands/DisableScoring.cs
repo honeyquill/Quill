de43356e0b7aca065abd05966d25992277feddde
Spike-LP <132815919+Spike-LP@users.noreply.github.com>
2026-02-14 19:51:16 +0100

    Added the ability to disable scoring
    Title but also made _IsQuillEnabled public as well as Isincustom

---
+++ Quill/Commands/DisableScoring.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Il2Cpp;
+using Unity.Netcode;
+using static Quill.Main;
+using HarmonyLib;
+using System.Reflection;
+namespace Quill
+{
+    public class DisableScoring : ChatCommand
+    {
+
+        public static void Init()
+        {
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("com.quill.DisableScoring");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        static bool AllowScoring = true;
+        public DisableScoring() : base("score", "Disables / Enables scoring", ChangeGoalAvaliability, 0)
+        {
+        }
+
+        private static void ChangeGoalAvaliability(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                AllowScoring = !AllowScoring;
+                if (AllowScoring) BeetleUtils.SendChatMessage("Scoring is now Enabled"); else BeetleUtils.SendChatMessage("Scoring is now Disabled");
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case string s when s == "on" || s == "1":
+                        AllowScoring = true;
+                        BeetleUtils.SendChatMessage("Scoring is now Enabled");
+                        break;
+                    case string s when s == "off" || s == "0":
+                        AllowScoring = false;
+                        BeetleUtils.SendChatMessage("Scoring is now Disabled");
+                        break;
+                    default:
+                        BeetleUtils.SendChatMessage("Usage: Score [on/off]");
+                        break;
+                }
+            }
+
+        }
+        [HarmonyPatch(typeof(Goal), "BallEnteredGoal_ServerRpc")]
+        class Goal_BallEnteredGoal_ServerRpc_Patch
+        {
+            static bool Prefix(ulong ballId)
+            {
+                if (Quill.Main._quillEnabled && Quill.Main.inCustom)
+                    return AllowScoring;
+                else
+                    return true;
+            }
+        }
+    }
+}
